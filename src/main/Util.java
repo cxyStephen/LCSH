66ec5b1b313d4fecc1a79c4deeaaf61f3ae78036
Stephen X Chen <cxystephen@gmail.com>
2019-03-29 18:00:02 -0400

    Added util functions to convert from rank to int and back (for calculating average rank)

--- src/main/Util.java
+++ src/main/Util.java
@@ -4,8 +4,17 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Util {
+
+    public static final String[] RANKS =
+            {"Iron", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master", "Grandmaster", "Challenger"};
+
+    public static final String[] DIVISIONS =
+            {"I", "II", "III", "IV"};
+
+    public static final int HIGH_ELO_INDEX = Arrays.asList(RANKS).indexOf("Master");
 
     public static InputStream getResource(String path) {
         return ClassLoader.getSystemResourceAsStream(path);
@@ -40,6 +49,49 @@ public class Util {
         double dpg = Math.round((factor * d)/games) / factor;
         double apg = Math.round((factor * a)/games) / factor;
         return kpg + "/" + dpg + "/" + apg;
+    }
+
+    public static int divisionToInt(String division) {
+        return Arrays.asList(DIVISIONS).indexOf(division);
+    }
+
+    public static int rankToInt(String rank, String division, int lp) {
+        int result = 0;
+
+        int rankInt = Arrays.asList(RANKS).indexOf(rank);
+        int divisionInt = divisionToInt(division);
+
+        if (rankInt >= HIGH_ELO_INDEX)
+            result += HIGH_ELO_INDEX * (DIVISIONS.length * 100) - 100; //extra 100 is accounted for by division 1
+        else if (rankInt == -1 || divisionInt == -1)
+            return -1;
+        else
+            result += rankInt * (DIVISIONS.length * 100);
+
+        result += (DIVISIONS.length - divisionToInt(division)) * 100;
+        result += lp;
+
+        return result;
+    }
+
+    public static String[] intToRank(int n) {
+        String[] result = new String[3];
+
+        if (n >= HIGH_ELO_INDEX * (DIVISIONS.length * 100)) {
+            result[0] = "Master+";
+            result[1] = "";
+            result[2] = (n - HIGH_ELO_INDEX * (DIVISIONS.length * 100)) + "";
+
+            return result;
+        }
+
+        int rankIndex = n/(DIVISIONS.length * 100);
+        int divisionIndex = DIVISIONS.length - ((n % (DIVISIONS.length * 100)) / 100);
+        result[0] = RANKS[rankIndex];
+        result[1] = DIVISIONS[divisionIndex];
+        result[2] = (n % 100) + "";
+
+        return result;
     }
 
     public static String readFile(String path) throws IOException {
